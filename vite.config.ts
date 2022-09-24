757ec27db48fd85907701b73fb91299e65188a21
Achel <rachel1665@icloud.com>
2022-09-24 15:05:01 +0800

    使用TSX代替Template

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: '/MoneyManager/dist/',    //部署到github时才用
-  plugins: [vue()]
+  plugins: [
+      vue(),
+    vueJsx({
+      transformOn: true,
+      mergeProps: true,
+    })
+  ]
 })
