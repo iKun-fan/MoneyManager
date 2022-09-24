b350f3656986614c0e1aa8deb32472395220fd13
Achel <rachel1665@icloud.com>
2022-09-24 14:18:24 +0800

    reset base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MoneyManager/dist/',
+  // base: '/MoneyManager/dist/',    //部署到github时才用
   plugins: [vue()]
 })
