dcbf18a7b1e312f89e94011241fc4cbdaa2ae5d4
Achel <rachel1665@icloud.com>
2022-09-24 15:39:04 +0800

    引入vue router

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import {App} from "./App";
+import {Foo} from "./pages/Foo";
+import {Bar} from "./pages/Bar";
+import {createRouter, createWebHashHistory} from "vue-router"
 
-createApp(App).mount('#app')
+const routes = [
+    {path: '/', component: Foo},
+    {path: '/about', component: Bar}
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
