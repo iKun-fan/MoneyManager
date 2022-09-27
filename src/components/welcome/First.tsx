c37daf972087723d5088169b34f105fca1ef70e6
Achel <rachel1665@icloud.com>
2022-09-27 15:12:44 +0800

    试验插槽用法

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,22 +1,18 @@
 import {defineComponent, isProxy} from "vue";
 import s from './First.module.scss'
 import {RouterLink} from "vue-router";
-import pig from '../../assets/icons/pig.svg'
+import pig from '../../assets/icons/pig.svg';
+import {Test} from './test';
 
 export const First = defineComponent({
     setup: (props, context) => {
+        const slots = {
+            icon: () => <span>icon</span>,
+            title: () => 'hi',
+            buttons: <><button>+1</button></>
+        }
         return () => (
-            <div class={s.wrapper}>
-                <div class={s.card}>
-                    <img class={s.icon} src={pig} alt="芝士图片"/>
-                    <h2>整点字<br/>再整点字</h2>
-                </div>
-                <div class={s.actions}>
-                    <RouterLink class={s.fake} to="/start">跳过</RouterLink>
-                    <RouterLink to="/welcome/2">下一页</RouterLink>
-                    <RouterLink to="start">跳过</RouterLink>
-                </div>
-            </div>
+            <Test v-slots={slots}></Test>
         )
     }
 })
