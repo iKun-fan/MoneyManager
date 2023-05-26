aea0fed1dd6049c6e4a4e615c3118847c8235c0c
Achel <rachel1665@icloud.com>
2023-05-26 12:18:21 +0800

    完成数字输入功能

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -15,7 +15,30 @@ export const InputPad = defineComponent({
         const refDate = ref<Date>(now)
 
         const refAmount = ref('')
-        const appendText = (n: number | string) => refAmount.value += n.toString()
+        const appendText = (n: number | string) => {
+            const nString = n.toString()
+            const dotIndex = refAmount.value.indexOf('.')
+            if (refAmount.value.length >= 13)
+                return
+
+            if (dotIndex >= 0 && refAmount.value.length - dotIndex > 2)
+                return
+
+            if (nString === '.') {
+                if (dotIndex >= 0) {
+                    return
+                }
+            } else if (nString === '0') {
+                if (refAmount.value === '0') {
+                    return
+                }
+            } else {
+                if (refAmount.value === '0') {
+                    refAmount.value = ''
+                }
+            }
+            refAmount.value += n.toString()
+        }
         const buttons = [
             {text: '1', onClick: () => {appendText(1)}},
             {text: '2', onClick: () => {appendText(2)}},
@@ -28,7 +51,7 @@ export const InputPad = defineComponent({
             {text: '9', onClick: () => {appendText(9)}},
             {text: '.', onClick: () => {appendText('.')}},
             {text: '0', onClick: () => {appendText(0)}},
-            {text: '清空', onClick: () => {}},
+            {text: '清空', onClick: () => {refAmount.value = '0'}},
             {text: '提交', onClick: () => {}},
         ]
 
