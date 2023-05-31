c570b1cdbf359c77eb7e2f57e6bf38a734412d9c
Achel <rachel1665@icloud.com>
2023-05-31 16:10:42 +0800

    完成TagEdit页面

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -1,13 +1,46 @@
-import {defineComponent, PropType} from "vue";
+import {defineComponent, PropType, reactive} from "vue";
+import {Rules, validate} from "../../shared/validate";
+import {MainLayout} from "../../layouts/MainLayout";
+import {Icon} from "../../shared/Icon";
+import {EmojiSelect} from "../../shared/EmojiSelect";
+import {Button} from "../../shared/Button";
+import s from "./Tag.module.scss"
+import {TagForm} from "./TagForm";
+
 export const TagEdit = defineComponent({
-    props: {
-        name: {
-            type: String as PropType<string>
-        }
-    },
     setup: (props, context) => {
+        const formData = reactive({
+            name: '',
+            sign: ''
+        })
+
+        const errors = reactive<{[k in keyof typeof formData] ?: string[]}>({})
+
+        const onSubmit = (e:Event) => {
+            const rules: Rules<typeof formData> = [
+                { key: 'name', type: 'required', message: '必填' },
+                { key: 'name', type: 'pattern', regex: /^.{1,4}$/, message: '只能填 1 到 4 个字符' },
+                { key: 'sign', type: 'required', message: '必填' },
+            ]
+            Object.assign(errors, {
+                name: undefined,
+                sign: undefined
+            })
+            Object.assign(errors, validate(formData, rules))
+            e.preventDefault()
+        }
         return () => (
-            <div>edit</div>
+            <MainLayout>{{
+                title: () => '新建标签',
+                icon: () => <Icon name="left" onClick={() => { }} />,
+                default: () => <>
+                    <TagForm />
+                    <div class={s.actions}>
+                        <Button level='danger' class={s.removeTags} onClick={() => { }}>删除标签</Button>
+                        <Button level='danger' class={s.removeTagsAndItems} onClick={() => { }}>删除标签和记账</Button>
+                    </div>
+                </>
+            }}</MainLayout>
         )
     }
 })
