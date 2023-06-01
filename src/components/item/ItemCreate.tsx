b21466302f3e61e26221845bb702cf6ceb7c3b9c
Achel <rachel1665@icloud.com>
2023-06-01 13:54:38 +0800

    拓展Tabs组件

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -14,62 +14,62 @@ export const ItemCreate = defineComponent({
     setup: (props, context) => {
         const refKind = ref('支出')
         const refExpensesTags = ref([
-            { id: 1, name: '餐费', sign: '￥', category: 'expenses' },
-            { id: 2, name: '打车', sign: '￥', category: 'expenses' },
-            { id: 3, name: '聚餐', sign: '￥', category: 'expenses' },
-            { id: 4, name: '打车', sign: '￥', category: 'expenses' },
-            { id: 5, name: '聚餐', sign: '￥', category: 'expenses' },
-            { id: 6, name: '打车', sign: '￥', category: 'expenses' },
-            { id: 7, name: '聚餐', sign: '￥', category: 'expenses' },
+            {id: 1, name: '餐费', sign: '￥', category: 'expenses'},
+            {id: 2, name: '打车', sign: '￥', category: 'expenses'},
+            {id: 3, name: '聚餐', sign: '￥', category: 'expenses'},
+            {id: 4, name: '打车', sign: '￥', category: 'expenses'},
+            {id: 5, name: '聚餐', sign: '￥', category: 'expenses'},
+            {id: 6, name: '打车', sign: '￥', category: 'expenses'},
+            {id: 7, name: '聚餐', sign: '￥', category: 'expenses'},
         ])
         const refIncomeTags = ref([
-            { id: 4, name: '工资', sign: '￥', category: 'income' },
-            { id: 5, name: '彩票', sign: '￥', category: 'income' },
-            { id: 6, name: '滴滴', sign: '￥', category: 'income' },
-            { id: 11, name: '彩票', sign: '￥', category: 'income' },
-            { id: 18, name: '滴滴', sign: '￥', category: 'income' },
-            { id: 17, name: '彩票', sign: '￥', category: 'income' },
-            { id: 19, name: '滴滴', sign: '￥', category: 'income' },
-            { id: 4, name: '工资', sign: '￥', category: 'income' },
-            { id: 5, name: '彩票', sign: '￥', category: 'income' },
-            { id: 6, name: '滴滴', sign: '￥', category: 'income' },
-            { id: 11, name: '彩票', sign: '￥', category: 'income' },
-            { id: 18, name: '滴滴', sign: '￥', category: 'income' },
-            { id: 17, name: '彩票', sign: '￥', category: 'income' },
-            { id: 19, name: '滴滴', sign: '￥', category: 'income' },
-            { id: 4, name: '工资', sign: '￥', category: 'income' },
-            { id: 5, name: '彩票', sign: '￥', category: 'income' },
-            { id: 6, name: '滴滴', sign: '￥', category: 'income' },
-            { id: 11, name: '彩票', sign: '￥', category: 'income' },
-            { id: 18, name: '滴滴', sign: '￥', category: 'income' },
-            { id: 17, name: '彩票', sign: '￥', category: 'income' },
-            { id: 19, name: '滴滴', sign: '￥', category: 'income' },
-            { id: 4, name: '工资', sign: '￥', category: 'income' },
-            { id: 5, name: '彩票', sign: '￥', category: 'income' },
-            { id: 6, name: '滴滴', sign: '￥', category: 'income' },
-            { id: 11, name: '彩票', sign: '￥', category: 'income' },
-            { id: 18, name: '滴滴', sign: '￥', category: 'income' },
-            { id: 17, name: '彩票', sign: '￥', category: 'income' },
-            { id: 19, name: '滴滴', sign: '￥', category: 'income' },
-            { id: 4, name: '工资', sign: '￥', category: 'income' },
-            { id: 5, name: '彩票', sign: '￥', category: 'income' },
-            { id: 6, name: '滴滴', sign: '￥', category: 'income' },
-            { id: 11, name: '彩票', sign: '￥', category: 'income' },
-            { id: 18, name: '滴滴', sign: '￥', category: 'income' },
-            { id: 17, name: '彩票', sign: '￥', category: 'income' },
-            { id: 19, name: '滴滴', sign: '￥', category: 'income' },
+            {id: 4, name: '工资', sign: '￥', category: 'income'},
+            {id: 5, name: '彩票', sign: '￥', category: 'income'},
+            {id: 6, name: '滴滴', sign: '￥', category: 'income'},
+            {id: 11, name: '彩票', sign: '￥', category: 'income'},
+            {id: 18, name: '滴滴', sign: '￥', category: 'income'},
+            {id: 17, name: '彩票', sign: '￥', category: 'income'},
+            {id: 19, name: '滴滴', sign: '￥', category: 'income'},
+            {id: 4, name: '工资', sign: '￥', category: 'income'},
+            {id: 5, name: '彩票', sign: '￥', category: 'income'},
+            {id: 6, name: '滴滴', sign: '￥', category: 'income'},
+            {id: 11, name: '彩票', sign: '￥', category: 'income'},
+            {id: 18, name: '滴滴', sign: '￥', category: 'income'},
+            {id: 17, name: '彩票', sign: '￥', category: 'income'},
+            {id: 19, name: '滴滴', sign: '￥', category: 'income'},
+            {id: 4, name: '工资', sign: '￥', category: 'income'},
+            {id: 5, name: '彩票', sign: '￥', category: 'income'},
+            {id: 6, name: '滴滴', sign: '￥', category: 'income'},
+            {id: 11, name: '彩票', sign: '￥', category: 'income'},
+            {id: 18, name: '滴滴', sign: '￥', category: 'income'},
+            {id: 17, name: '彩票', sign: '￥', category: 'income'},
+            {id: 19, name: '滴滴', sign: '￥', category: 'income'},
+            {id: 4, name: '工资', sign: '￥', category: 'income'},
+            {id: 5, name: '彩票', sign: '￥', category: 'income'},
+            {id: 6, name: '滴滴', sign: '￥', category: 'income'},
+            {id: 11, name: '彩票', sign: '￥', category: 'income'},
+            {id: 18, name: '滴滴', sign: '￥', category: 'income'},
+            {id: 17, name: '彩票', sign: '￥', category: 'income'},
+            {id: 19, name: '滴滴', sign: '￥', category: 'income'},
+            {id: 4, name: '工资', sign: '￥', category: 'income'},
+            {id: 5, name: '彩票', sign: '￥', category: 'income'},
+            {id: 6, name: '滴滴', sign: '￥', category: 'income'},
+            {id: 11, name: '彩票', sign: '￥', category: 'income'},
+            {id: 18, name: '滴滴', sign: '￥', category: 'income'},
+            {id: 17, name: '彩票', sign: '￥', category: 'income'},
+            {id: 19, name: '滴滴', sign: '￥', category: 'income'},
         ])
         return () => (
             <MainLayout class={s.layout}>{{
                 title: () => '记一笔',
-                icon: () => <Icon name="left" class={s.navIcon} />,
+                icon: () => <Icon name="left" class={s.navIcon}/>,
                 default: () => <>
                     <div class={s.wrapper}>
                         <Tabs v-model:selected={refKind.value} class={s.tabs}>
                             <Tab name="支出" class={s.tags_wrapper}>
                                 <div class={s.tag}>
                                     <div class={s.sign}>
-                                        <Icon name="add" class={s.createTag} />
+                                        <Icon name="add" class={s.createTag}/>
                                     </div>
                                     <div class={s.name}>
                                         新增
@@ -89,7 +89,7 @@ export const ItemCreate = defineComponent({
                             <Tab name="收入" class={s.tags_wrapper}>
                                 <div class={s.tag}>
                                     <div class={s.sign}>
-                                        <Icon name="add" class={s.createTag} />
+                                        <Icon name="add" class={s.createTag}/>
                                     </div>
                                     <div class={s.name}>
                                         新增
@@ -108,7 +108,7 @@ export const ItemCreate = defineComponent({
                             </Tab>
                         </Tabs>
                         <div class={s.inputPad_wrapper}>
-                            <InputPad />
+                            <InputPad/>
                         </div>
                     </div>
                 </>
--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -1,5 +1,8 @@
-import {defineComponent, PropType} from "vue";
+import {defineComponent, PropType, ref} from "vue";
 import s from './ItemList.module.scss'
+import {MainLayout} from "../../layouts/MainLayout";
+import {Icon} from "../../shared/Icon";
+import {Tab, Tabs} from "../../shared/Tabs"
 
 export const ItemList = defineComponent({
     props: {
@@ -8,8 +11,30 @@ export const ItemList = defineComponent({
         }
     },
     setup: (props, context) => {
+        const refSelected = ref('本月')
         return () => (
-            <div class={s.wrapper}>hello</div>
+            <MainLayout>{
+                {
+                    title: () => '山竹记账',
+                    icon: () => <Icon name="menu" />,
+                    default: () => (
+                        <Tabs classPrefix={'customTabs'} v-model:selected={refSelected.value}>
+                            <Tab name="本月">
+                                list 1
+                            </Tab>
+                            <Tab name="上月">
+                                list 2
+                            </Tab>
+                            <Tab name="今年">
+                                list 3
+                            </Tab>
+                            <Tab name="自定义时间">
+                                list 4
+                            </Tab>
+                        </Tabs>
+                    )
+                }
+            }</MainLayout>
         )
     }
 })
