8866a153a49e8f020d040a516ff1b2c9a900044d
Achel <rachel1665@icloud.com>
2023-06-01 14:22:30 +0800

    重构time函数为Time类

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,7 +1,7 @@
 import {defineComponent, PropType, ref} from "vue";
 import s from  './InputPad.module.scss';
 import {Icon} from "../../shared/Icon";
-import {time} from "../../shared/time";
+import {Time} from "../../shared/time";
 import {DatetimePicker, Popup, NumberKeyboard} from "vant";
 import 'vant/lib/index.css';
 export const InputPad = defineComponent({
@@ -64,7 +64,7 @@ export const InputPad = defineComponent({
                 <span class={s.date}>
                     <Icon name="date" class={s.icon}/>
                     <span>
-                        <span onClick={showDatePicker}>{time(refDate.value).format()}</span>
+                        <span onClick={showDatePicker}>{new Time(refDate.value).format()}</span>
                         <Popup position='bottom' v-model:show={refDatePickerVisible.value}>
                             <DatetimePicker value={refDate.value} type="date" title="选择年月日" onConfirm={setDate} onCancel={hideDatePicker}/>
                         </Popup>
