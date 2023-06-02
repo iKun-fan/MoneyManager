b886292171401c2ec0f23ca288f8644d22928075
Achel <rachel1665@icloud.com>
2023-06-02 14:18:46 +0800

    完成登录页面样式

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -15,6 +15,7 @@ import {ItemCreate} from "../components/item/ItemCreate"
 import {TagPage} from "../pages/TagPage";
 import {TagCreate} from "../components/tag/TagCreate";
 import {TagEdit} from "../components/tag/TagEdit";
+import {SignInPage} from "../pages/SignInPage";
 
 export const routes: RouteRecordRaw[] = [
     {path: '/', redirect: '/welcome'},
@@ -43,5 +44,8 @@ export const routes: RouteRecordRaw[] = [
             {path: 'create', component: TagCreate},
             {path: 'id/edit', component: TagEdit}
         ]
+    },
+    {
+        path: '/sign_in', component: SignInPage
     }
 ]
