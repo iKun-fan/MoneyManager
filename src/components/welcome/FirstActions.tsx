99011d325232d9ec9992e6169a5c7eca7b698a01
Achel <rachel1665@icloud.com>
2023-06-07 19:32:58 +0800

    看过广告不用再看

--- src/components/welcome/FirstActions.tsx
+++ src/components/welcome/FirstActions.tsx
@@ -1,12 +1,13 @@
 import {FunctionalComponent} from "vue";
 import s from './WelcomeLayout.module.scss'
 import {RouterLink} from "vue-router";
+import {SkipFeatures} from "../../shared/SkipFeatures";
 
 export const FirstActions: FunctionalComponent = () => {
     return <div class={s.actions}>
-        <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
+        <SkipFeatures class={s.fake}/>
         <RouterLink to="/welcome/2" >下一页</RouterLink>
-        <RouterLink to="/start" >跳过</RouterLink>
+        <SkipFeatures/>
     </div>
 }
 
--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -67,7 +67,6 @@ export const SignInPage = defineComponent({
                                 <Icon class={s.icon} name="mangosteen" />
                                 <h1 class={s.appName}>山竹记账</h1>
                             </div>
-                            <div>{JSON.stringify(formData)}</div>
                             <Form onSubmit={onSubmit}>
                                 <FormItem label="邮箱地址" type="text" placeholder='请输入邮箱，然后点击发送验证码' v-model={formData.email} error={errors.email?.[0]} />
                                 <FormItem ref={refValidationCode} label="验证码" type="validationCode" onClick={onClickSendValidationCode} placeholder='请输入六位验证码' countFrom={60} disabled={refDisabled.value} v-model={formData.code} error={errors.code?.[0]} />
