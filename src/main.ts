e0b29f81ca78753059b508f815028725db84eb04
Achel <rachel1665@icloud.com>
2023-06-07 19:51:30 +0800

    每个路由做登录检测

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import {App} from "./App";
 import {createRouter} from "vue-router"
 import {routes} from "./config/routes";
 import {history} from "./shared/history";
 import '@svgstore'
+import {fetchMe, mePromise} from "./shared/me";
 
 const router = createRouter({history, routes,})
+
+fetchMe()
+
+router.beforeEach(async (to, from) => {
+    if (to.path === '/' || to.path.startsWith('/welcome') || to.path.startsWith('/sign_in') || to.path === '/start') {
+        return true
+    } else {
+        return await mePromise!.then(
+            () => true,
+            () => 'sign_in?return_to=' + to.path
+        )
+    }
+})
 
 const app = createApp(App)
 app.use(router)
