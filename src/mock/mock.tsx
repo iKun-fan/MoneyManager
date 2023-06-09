6b43a14ee32267f6b68ebd931ecc44fb31be9880
Achel <rachel1665@icloud.com>
2023-06-09 19:57:58 +0800

    初步转换折线图数据

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -3,6 +3,17 @@ import {fakerZH_CN as faker} from "@faker-js/faker";
 
 type Mock = (config: AxiosRequestConfig) => [number, any]
 
+export const mockItemSummary: Mock = config => {
+    return [200, {
+        "groups": [
+            { "happen_at": "2018-06-18T00:00:00.000+0800", "amount": 100 },
+            { "happen_at": "2018-06-22T00:00:00.000+0800", "amount": 300 },
+            { "happen_at": "2018-06-29T00:00:00.000+0800", "amount": 200 }
+        ],
+        "summary": 600
+    }]
+}
+
 export const mockItemIndexBalance: Mock = config => {
     return [200, {
         expenses: 9900,
