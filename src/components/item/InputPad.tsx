b63bab376542ad5750ec8b0ea4c69e8f9d50ed88
Achel <rachel1665@icloud.com>
2023-06-09 16:50:40 +0800

    可以新增记账了

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -7,7 +7,10 @@ import 'vant/lib/index.css';
 export const InputPad = defineComponent({
     props: {
         happenAt: String,
-        amount: Number
+        amount: Number,
+        onSubmit: {
+            type: Function as PropType<() => void>
+        }
     },
     setup: (props, context) => {
         const appendText = (n: number | string) => {
@@ -47,7 +50,13 @@ export const InputPad = defineComponent({
             {text: '.', onClick: () => {appendText('.')}},
             {text: '0', onClick: () => {appendText(0)}},
             {text: '清空', onClick: () => {refAmount.value = '0'}},
-            {text: '提交', onClick: () => context.emit('update:amount', parseFloat(refAmount.value) * 100) },
+            {
+                text: '提交',
+                onClick: () => {
+                    context.emit('update:amount', parseFloat(refAmount.value) * 100)
+                    props.onSubmit?.()
+                }
+            },
         ]
 
         const refDatePickerVisible = ref<boolean>(false)
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,6 +6,11 @@ declare module '*.vue' {
   export default component
 }
 
+declare module '*.scss' {
+  const content: Record<string, any> = {}
+  export default content
+}
+
 type JSONValue = null | boolean | string | number | JSONValue[] | Record<string, JSONValue>
 
 type Tag = {
@@ -16,6 +21,15 @@ type Tag = {
   kind: expenses | income
 }
 
+type Item = {
+  id: number
+  user_id: number
+  amount: number
+  tags_id:number[]
+  happen_at: string
+  kind: expenses | income
+}
+
 type Resources<T = any> = {
   resources: T[]
   pager: {
@@ -23,4 +37,12 @@ type Resources<T = any> = {
     per_page: number,
     count: number
   }
+}
+
+type Resource<T> = {
+  resource: T
+}
+
+type ResourceError = {
+  errors: Record<string, string[]>
 }
