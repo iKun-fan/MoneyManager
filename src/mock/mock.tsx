c8dbcc611dd21803ca190c8f9242d5d66693c948
Achel <rachel1665@icloud.com>
2023-06-09 20:24:01 +0800

    完成饼图

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -3,15 +3,32 @@ import {fakerZH_CN as faker} from "@faker-js/faker";
 
 type Mock = (config: AxiosRequestConfig) => [number, any]
 
-export const mockItemSummary: Mock = config => {
-    return [200, {
-        "groups": [
-            { "happen_at": "2022-07-18T00:00:00.000+0800", "amount": 100 },
-            { "happen_at": "2022-07-22T00:00:00.000+0800", "amount": 300 },
-            { "happen_at": "2022-07-29T00:00:00.000+0800", "amount": 200 }
-        ],
-        "summary": 600
-    }]
+export const mockItemSummary: Mock = (config) => {
+    if (config.params.group_by === 'happen_at') {
+        return [
+            200,
+            {
+                groups: [
+                    { happen_at: '2022-07-18T00:00:00.000+0800', amount: 100 },
+                    { happen_at: '2022-07-22T00:00:00.000+0800', amount: 300 },
+                    { happen_at: '2022-07-29T00:00:00.000+0800', amount: 200 }
+                ],
+                summary: 600
+            }
+        ]
+    } else {
+        return [
+            200,
+            {
+                groups: [
+                    { tag_id: 1, tag: { id: 1, name: '交通' }, amount: 100 },
+                    { tag_id: 2, tag: { id: 2, name: '吃饭' }, amount: 300 },
+                    { tag_id: 3, tag: { id: 3, name: '购物' }, amount: 200 }
+                ],
+                summary: 600
+            }
+        ]
+    }
 }
 
 export const mockItemIndexBalance: Mock = config => {
@@ -29,16 +46,16 @@ export const mockItemIndex: Mock = (config) => {
     const createPaper = (page = 1) => ({
         page,
         per_page,
-        count,
+        count
     })
-    const createTag = (attrs?: any) =>
-        ({
-            id: createId(),
-            name: faker.lorem.word(),
-            sign: faker.internet.emoji(),
-            kind: 'expenses',
-            ...attrs
-        })
+    const createTag = (attrs?: any) => ({
+        id: createId(),
+        name: faker.lorem.word(),
+        sign: faker.internet.emoji(),
+        kind: 'expenses',
+        ...attrs
+    })
+
     const createItem = (n = 1, attrs?: any) =>
         Array.from({ length: n }).map(() => ({
             id: createId(),
