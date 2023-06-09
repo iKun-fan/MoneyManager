71c2a7b6a54c445c5b5500ee881881f16bad958c
Achel <rachel1665@icloud.com>
2023-06-10 00:25:22 +0800

    tags_id 改为 tag_ids

--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -42,7 +42,9 @@ export class Http {
 }
 
 const mock = (response: AxiosResponse) => {
-    if (location.hostname !== 'localhost'
+    // 加ture禁用mock
+    if (true ||
+        location.hostname !== 'localhost'
         && location.hostname !== '127.0.0.1'
         && location.hostname !== '192.168.3.57') {
         return false
