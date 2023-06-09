f091b17a931d5250fe7f95fc7c5d57855799b0d3
Achel <rachel1665@icloud.com>
2023-06-09 17:33:57 +0800

    mock tagEdit

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -3,6 +3,18 @@ import {fakerZH_CN as faker} from "@faker-js/faker";
 
 type Mock = (config: AxiosRequestConfig) => [number, any]
 
+export const mockTagEdit: Mock = config => {
+    const createTag = (attrs?: any) =>
+        ({
+            id: createId(),
+            name: faker.lorem.word(),
+            sign: faker.internet.emoji(),
+            kind: 'expenses',
+            ...attrs
+        })
+    return [200, {resource: createTag()}]
+}
+
 export const mockTagShow: Mock = config =>{
     const createTag = (attrs?: any) =>
         ({
