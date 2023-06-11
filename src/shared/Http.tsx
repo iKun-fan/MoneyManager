736c929c68f38f698baf3e6c9b801934ff3d0fea
Achel <rachel1665@icloud.com>
2023-06-11 19:13:33 +0800

    访问8080端口

--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -78,7 +78,7 @@ const mock = (response: AxiosResponse) => {
     return false
 }
 
-export const http = new Http('/api/v1')
+export const http = new Http('http://121.196.236.94:8080/api/v1')
 
 http.instance.interceptors.request.use(config => {
     const jwt = localStorage.getItem('jwt')
