21c0c91886a82401347a10f99cb3e99f86495a41
Achel <rachel1665@icloud.com>
2023-06-11 15:38:01 +0800

    使用 store.$reset() 代替 reset

--- src/stores/useItemStore.tsx
+++ src/stores/useItemStore.tsx
@@ -7,8 +7,9 @@ type State = {
     page: number
 }
 type Actions = {
-    reset: () => void
+    _fetch: (firstPage: boolean, startDate?: string, endDate?: string) => void
     fetchItems: (startDate?: string, endDate?: string) => void
+    fetchNextPage: (startDate?: string, endDate?: string) => void
 }
 export const useItemStore = (id: string | string[]) =>
     defineStore<string, State, {}, Actions>(typeof id === 'string' ? id : id.join('-'), {
@@ -18,12 +19,7 @@ export const useItemStore = (id: string | string[]) =>
             page: 0
         }),
         actions: {
-            reset() {
-                this.items = []
-                this.hasMore = false
-                this.page = 0
-            },
-            async fetchItems(startDate, endDate) {
+            async _fetch(firstPage, startDate, endDate) {
                 if (!startDate || !endDate) {
                     return
                 }
@@ -32,7 +28,7 @@ export const useItemStore = (id: string | string[]) =>
                     {
                         happen_after: startDate,
                         happen_before: endDate,
-                        page: this.page + 1
+                        page: firstPage ? 1 : this.page + 1
                     },
                     {
                         _mock: 'itemIndex',
@@ -40,9 +36,19 @@ export const useItemStore = (id: string | string[]) =>
                     }
                 )
                 const { resources, pager } = response.data
-                this.items?.push(...resources)
+                if (firstPage) {
+                    this.items = resources
+                } else {
+                    this.items.push(...resources)
+                }
                 this.hasMore = (pager.page - 1) * pager.per_page + resources.length < pager.count
                 this.page += 1
+            },
+            async fetchNextPage(startDate, endDate) {
+                this._fetch(false, startDate, endDate)
+            },
+            async fetchItems(startDate, endDate) {
+                this._fetch(true, startDate, endDate)
             }
         }
     })()
