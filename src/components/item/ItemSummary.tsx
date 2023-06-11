b17e6b84f1d9838f31ad4f214ebf2818fc56bf4f
Achel <rachel1665@icloud.com>
2023-06-11 15:15:37 +0800

    封装useAfterMe

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -8,6 +8,7 @@ import {Datetime} from "../../shared/Datetime";
 import {Icon} from "../../shared/Icon";
 import {RouterLink} from "vue-router";
 import {Center} from "../../shared/Center";
+import {useAfterMe} from "../../hooks/useAfterMe";
 
 export const ItemSummary = defineComponent({
     props: {
@@ -39,7 +40,7 @@ export const ItemSummary = defineComponent({
             hasMore.value = (pager.page - 1) * pager.per_page + resources.length < pager.count
             page.value += 1
         }
-        onMounted(fetchItems)
+        useAfterMe(fetchItems)
 
         watch(()=>[props.startDate,props.endDate], ()=>{
             items.value = []
@@ -64,7 +65,7 @@ export const ItemSummary = defineComponent({
             })
             Object.assign(itemsBalance, response.data)
         }
-        onMounted(fetchItemsBalance)
+        useAfterMe(fetchItemsBalance)
         watch(()=>[props.startDate,props.endDate], ()=>{
             Object.assign(itemsBalance, {
                 expenses: 0, income: 0, balance: 0
---
+++ src/hooks/useAfterMe.tsx
@@ -0,0 +1,10 @@
+import {useMeStore} from "../stores/useMeStore";
+import {onMounted} from "vue";
+
+export const useAfterMe = (fn: () => void) => {
+    const meStore = useMeStore()
+    onMounted(async () => {
+        await meStore.mePromise
+        fn()
+    })
+}
